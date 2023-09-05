5e259474651e09b6eb2c665084fdef929f81aafb
Nik Graf <nik@nikgraf.com>
2023-09-05 22:18:16 +0200

    chore: reenable logging for documentation example

--- documentation/components/YjsProsemirrorExample.tsx
+++ documentation/components/YjsProsemirrorExample.tsx
@@ -85,7 +85,7 @@ const YjsProsemirrorExample: React.FC<Props> = ({
       return true;
     },
     sodium,
-    logging: "off", // TODO
+    logging: "debug",
   });
 
   const initiateEditor = () => {
