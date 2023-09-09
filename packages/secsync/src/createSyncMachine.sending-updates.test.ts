75ae307e715f354d6a073978a22dd9854349b12f
Nik Graf <nik@nikgraf.com>
2023-09-09 18:33:04 +0200

    test: add sending updates tests

---
+++ packages/secsync/src/createSyncMachine.sending-updates.test.ts
@@ -0,0 +1,237 @@
+import sodium, { KeyPair } from "libsodium-wrappers";
+import { assign, interpret, spawn } from "xstate";
+import { createSyncMachine } from "./createSyncMachine";
+import { generateId } from "./crypto/generateId";
+import { createSnapshot } from "./snapshot/createSnapshot";
+import {
+  SnapshotPublicData,
+  SyncMachineConfig,
+  UpdatePublicData,
+} from "./types";
+import { createUpdate } from "./update/createUpdate";
+
+const url = "wss://www.example.com";
+
+let clientAKeyPair: KeyPair;
+let clientAPublicKey: string;
+
+let clientBKeyPair: KeyPair;
+let clientBPublicKey: string;
+
+let key: Uint8Array;
+let docId: string;
+let snapshotId: string;
+
+beforeEach(async () => {
+  await sodium.ready;
+
+  docId = generateId(sodium);
+
+  clientAKeyPair = {
+    privateKey: sodium.from_base64(
+      "g3dtwb9XzhSzZGkxTfg11t1KEIb4D8rO7K54R6dnxArvgg_OzZ2GgREtG7F5LvNp3MS8p9vsio4r6Mq7SZDEgw"
+    ),
+    publicKey: sodium.from_base64(
+      "74IPzs2dhoERLRuxeS7zadzEvKfb7IqOK-jKu0mQxIM"
+    ),
+    keyType: "ed25519",
+  };
+  clientAPublicKey = sodium.to_base64(clientAKeyPair.publicKey);
+
+  clientBKeyPair = {
+    privateKey: sodium.from_base64(
+      "ElVI9nkbOypSu2quCTXH1i1gGlcd-Sxd7S6ym9sNZj48ben-hOmefr13D9Y1Lnys3CuhwuPb6DMh_oDln913_g"
+    ),
+    publicKey: sodium.from_base64(
+      "PG3p_oTpnn69dw_WNS58rNwrocLj2-gzIf6A5Z_dd_4"
+    ),
+    keyType: "ed25519",
+  };
+  clientBPublicKey = sodium.to_base64(clientBKeyPair.publicKey);
+});
+
+type CreateSnapshotTestHelperParams = {
+  parentSnapshotCiphertext: string;
+  grandParentSnapshotProof: string;
+  content: string;
+  // Note: lacks the `parentSnapshotClocks` param from other test suites
+};
+
+const createSnapshotTestHelper = (params?: CreateSnapshotTestHelperParams) => {
+  snapshotId = generateId(sodium);
+  const { parentSnapshotCiphertext, grandParentSnapshotProof, content } =
+    params || {};
+  key = sodium.from_hex(
+    "724b092810ec86d7e35c9d067702b31ef90bc43a7b598626749914d6a3e033ed"
+  );
+
+  const publicData: SnapshotPublicData = {
+    snapshotId,
+    docId: "6e46c006-5541-11ec-bf63-0242ac130002",
+    pubKey: clientAPublicKey,
+    parentSnapshotClocks: {},
+  };
+
+  const snapshot = createSnapshot(
+    content || "Hello World",
+    publicData,
+    key,
+    clientAKeyPair,
+    parentSnapshotCiphertext || "",
+    grandParentSnapshotProof || "",
+    sodium
+  );
+  return {
+    snapshot: {
+      ...snapshot,
+      serverData: { latestVersion: 0 },
+    },
+    key,
+    clientAKeyPair,
+  };
+};
+
+type CreateUpdateTestHelperParams = {
+  version: number;
+  signatureKeyPair?: KeyPair;
+};
+
+const createUpdateTestHelper = (params?: CreateUpdateTestHelperParams) => {
+  const version = params?.version || 0;
+  const signatureKeyPair = params?.signatureKeyPair || clientAKeyPair;
+  const publicData: UpdatePublicData = {
+    refSnapshotId: snapshotId,
+    docId,
+    pubKey: sodium.to_base64(signatureKeyPair.publicKey),
+  };
+
+  const update = createUpdate(
+    "u",
+    publicData,
+    key,
+    signatureKeyPair,
+    version,
+    sodium
+  );
+
+  return { update: { ...update, serverData: { version } } };
+};
+
+test.only("put changes in updatesInFlight when sending updates", (done) => {
+  const websocketServiceMock =
+    (context: SyncMachineConfig) => (send: any, onReceive: any) => {
+      onReceive((event: any) => {});
+
+      send({ type: "WEBSOCKET_CONNECTED" });
+
+      return () => {};
+    };
+
+  let docValue = "";
+  let transitionCount = 0;
+
+  const syncMachine = createSyncMachine();
+  const syncService = interpret(
+    syncMachine
+      .withContext({
+        ...syncMachine.context,
+        websocketHost: url,
+        websocketSessionKey: "sessionKey",
+        isValidCollaborator: (signingPublicKey) =>
+          clientAPublicKey === signingPublicKey ||
+          clientBPublicKey === signingPublicKey,
+        getSnapshotKey: () => key,
+        getNewSnapshotData: async () => {
+          return {
+            data: "New Snapshot Data",
+            id: generateId(sodium),
+            key,
+            publicData: {},
+          };
+        },
+        applySnapshot: (snapshot) => {
+          docValue = sodium.to_string(snapshot);
+        },
+        getUpdateKey: () => key,
+        sodium: sodium,
+        signatureKeyPair: clientBKeyPair,
+        logging: "error",
+      })
+      .withConfig({
+        actions: {
+          spawnWebsocketActor: assign((context) => {
+            return {
+              _websocketActor: spawn(
+                websocketServiceMock(context),
+                "websocketActor"
+              ),
+            };
+          }),
+        },
+      })
+  );
+
+  const runEvents = () => {
+    const { snapshot } = createSnapshotTestHelper();
+    syncService.send({
+      type: "WEBSOCKET_ADD_TO_INCOMING_QUEUE",
+      data: {
+        type: "document",
+        snapshot,
+      },
+    });
+
+    // TODO check for this case where changes are added before the document is processed?
+    // syncService.send({
+    //   type: "ADD_CHANGES",
+    //   data: ["H", "e"],
+    // });
+    // syncService.send({
+    //   type: "ADD_CHANGES",
+    //   data: ["llo"],
+    // });
+
+    setTimeout(() => {
+      syncService.send({
+        type: "ADD_CHANGES",
+        data: ["H", "e"],
+      });
+      syncService.send({
+        type: "ADD_CHANGES",
+        data: ["llo"],
+      });
+
+      setTimeout(() => {
+        syncService.send({
+          type: "ADD_CHANGES",
+          data: ["World"],
+        });
+      }, 1);
+    }, 1);
+  };
+
+  syncService.onTransition((state, event) => {
+    transitionCount = transitionCount + 1;
+    if (event.type === "WEBSOCKET_CONNECTED") {
+      runEvents();
+    }
+
+    if (state.context._updatesInFlight.length === 1) {
+      expect(state.context._updatesInFlight).toStrictEqual([
+        { clock: 0, changes: ["H", "e", "llo"] },
+      ]);
+    } else if (
+      state.context._updatesInFlight.length === 2 &&
+      state.matches("connected.idle")
+    ) {
+      expect(state.context._updatesInFlight).toStrictEqual([
+        { clock: 0, changes: ["H", "e", "llo"] },
+        { clock: 1, changes: ["World"] },
+      ]);
+      expect(state.context._pendingChangesQueue).toEqual([]);
+      done();
+    }
+  });
+
+  syncService.start();
+});
