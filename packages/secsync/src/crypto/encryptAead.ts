dbf86183c812c40aa073d781252f0947f0fac4aa
Nik Graf <nik@nikgraf.com>
2023-11-23 16:05:21 +0100

    add aead robustness as recommend by libsoidum docs

--- packages/secsync/src/crypto/encryptAead.ts
+++ packages/secsync/src/crypto/encryptAead.ts
@@ -1,29 +1,34 @@
-import { prefixWithUint8Array } from "../utils/prefixWithUint8Array";
-
 export function encryptAead(
   message: Uint8Array | string,
   additionalData: string,
   key: Uint8Array,
   sodium: typeof import("libsodium-wrappers")
 ) {
-  const publicNonce = sodium.randombytes_buf(
+  const publicNonceUint8Array = sodium.randombytes_buf(
     sodium.crypto_aead_xchacha20poly1305_ietf_NPUBBYTES
   );
-  const result = {
-    publicNonce: sodium.to_base64(publicNonce),
-    ciphertext: sodium.to_base64(
-      sodium.crypto_aead_xchacha20poly1305_ietf_encrypt(
-        // prefixing with a block of null bytes to commit to a single (plaintext, AAD)
-        // pair that result in the same (ciphertext, authentication tag) pair
-        // see https://soatok.blog/2023/04/03/asymmetric-cryptographic-commitments/#what-is-commitment
-        // and https://eprint.iacr.org/2019/016
-        prefixWithUint8Array(message, new Uint8Array([0, 0, 0, 0])),
-        additionalData,
-        null,
-        publicNonce,
-        key
-      )
-    ),
+  const publicNonce = sodium.to_base64(publicNonceUint8Array);
+  const ciphertext = sodium.crypto_aead_xchacha20poly1305_ietf_encrypt(
+    message,
+    additionalData,
+    null,
+    publicNonceUint8Array,
+    key
+  );
+
+  const robustnessTag = sodium.crypto_auth(
+    publicNonce + sodium.to_base64(ciphertext) + additionalData,
+    key
+  );
+
+  const finalCiphertext = new Uint8Array(
+    robustnessTag.length + ciphertext.length
+  );
+  finalCiphertext.set(robustnessTag);
+  finalCiphertext.set(ciphertext, robustnessTag.length);
+
+  return {
+    publicNonce,
+    ciphertext: sodium.to_base64(finalCiphertext),
   };
-  return result;
 }
